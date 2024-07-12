01ca8120c9559f137f541004746e7cecac410393
salvatorepancucci <salvatore.pancucci@gmail.com>
2024-07-12 14:13:49 +0200

    Objects array added

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// Array di post di esempio
+const posts = [
+    { id: 1, content: "Primo post!", likes: 0 },
+    { id: 2, content: "Secondo post!", likes: 0 },
+    { id: 3, content: "Terzo post!", likes: 0 }
+];
