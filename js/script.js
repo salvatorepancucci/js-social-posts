94a7bb6506e259c5b34fc370255390055edf3291
salvatorepancucci <salvatore.pancucci@gmail.com>
2024-07-12 14:14:11 +0200

    Like array added

--- js/script.js
+++ js/script.js
@@ -4,3 +4,6 @@ const posts = [
     { id: 2, content: "Secondo post!", likes: 0 },
     { id: 3, content: "Terzo post!", likes: 0 }
 ];
+
+// Array per memorizzare gli id dei post ai quali abbiamo messo il like
+let likedPosts = [];
